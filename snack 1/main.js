653019f41a1b2a35278450b87e490779ce3fb11f
gialli24 <a.gialli2409@gmail.com>
2026-02-05 10:54:32 +0100

    Rename variables

--- snack 1/main.js
+++ snack 1/main.js
@@ -1,13 +1,13 @@
-const n = parseInt(prompt("Inserisci un primo numero"));
-const m = parseInt(prompt("Inserisci un secondo numero"));
+const first_input = parseInt(prompt("Inserisci un primo numero"));
+const second_input = parseInt(prompt("Inserisci un secondo numero"));
 
-console.log('n : ' + n , 'm : ' + m);
+console.log('n : ' + first_input , 'm : ' + second_input);
 
 
-if (n < m) {
-    console.log(`Il numero maggiore è : ${m}`);
-} else if (n > m) {
-    console.log(`Il numero maggiore è : ${n}`);
+if (first_input < second_input) {
+    console.log(`Il numero maggiore è : ${second_input}`);
+} else if (first_input > second_input) {
+    console.log(`Il numero maggiore è : ${first_input}`);
 } else {
     console.log("I numeri sono uguali");
 }
