5b091d793d148e9750d6999ada4ec2561a63f5de
gialli24 <a.gialli2409@gmail.com>
2026-02-05 10:45:53 +0100

    Add parseint()

--- snack 1/main.js
+++ snack 1/main.js
@@ -1,13 +1,13 @@
-const n = prompt("Inserisci un primo numero");
-const m = prompt("Inserisci un secondo numero");
+const n = parseInt(prompt("Inserisci un primo numero"));
+const m = parseInt(prompt("Inserisci un secondo numero"));
 
 console.log('n : ' + n , 'm : ' + m);
 
 
 if (n < m) {
-    console.log(`Il numero maggiore è : ${n}`);
-} else if (n > m) {
     console.log(`Il numero maggiore è : ${m}`);
+} else if (n > m) {
+    console.log(`Il numero maggiore è : ${n}`);
 } else {
     console.log("I numeri sono uguali");
 }
